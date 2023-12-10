f3ac36f26ea7ca93af9cc2bf0652038f8b7e320e
lladdy <randallboland@bigpond.com>
2023-12-10 20:08:12 +1030

    chore: update project name

--- projectperseus/urls.py
+++ projectperseus/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for revitsynctest project.
+URL configuration for projectperseus project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
