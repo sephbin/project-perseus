305b2199bd5a6aff3031306e6d8760810ade19d3
lladdy <randallboland@bigpond.com>
2023-12-13 15:41:37 +1030

    fix: root path returning 403

--- projectperseus/urls.py
+++ projectperseus/urls.py
@@ -24,7 +24,7 @@ router = routers.DefaultRouter()
 router.register(r'elements', views.ElementViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('rapi/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', admin.site.urls),
 ]
