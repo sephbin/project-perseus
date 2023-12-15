8d0087d0de171a74e41cfcb213bcb44f7e3bb4be
lladdy <randallboland@bigpond.com>
2023-12-15 19:24:35 +1030

    chore: cleanup commented code

--- web_api/views.py
+++ web_api/views.py
@@ -21,14 +21,4 @@ class ElementViewSet(viewsets.ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=201, headers=headers)
 
-    # def update(self, request, *args, **kwargs):
-    #     data = request.data if isinstance(request.data, list) else [request.data]
-    #
-    #     serializer = self.create_or_update_serializer_class(data=data)
-    #     serializer.is_valid(raise_exception=True)
-    #     self.perform_update(serializer)
-    #
-    #     headers = self.get_success_headers(serializer.data)
-    #     return Response(serializer.data, status=200, headers=headers)
-
 
