f9af8bf183418bf910735902740055cc88c4366e
lladdy <randallboland@bigpond.com>
2023-12-15 19:59:33 +1030

    feat: add uniqueid

--- core/models.py
+++ core/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 class Element(models.Model):
     id = models.BigIntegerField(primary_key=True)
+    unique_id = models.UUIDField(unique=True)
     name = models.TextField(blank=True)
     comments = models.TextField(blank=True)
