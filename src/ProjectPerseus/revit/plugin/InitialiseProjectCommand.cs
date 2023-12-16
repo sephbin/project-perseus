093ec159d42720459dcd7c80d0a10a8cf69630b6
lladdy <randallboland@bigpond.com>
2023-12-16 14:45:20 +1030

    chore: add skeleton init project cmd

---
+++ src/ProjectPerseus/revit/plugin/InitialiseProjectCommand.cs
@@ -0,0 +1,18 @@
+﻿
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace ProjectPerseus
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class InitialiseProjectCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // todo
+            return Result.Succeeded;
+        }
+    }
+}
