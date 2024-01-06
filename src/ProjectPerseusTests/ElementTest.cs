ae931e30f8ca8a2996ab8cfc57a50721925d9a89
lladdy <randallboland@bigpond.com>
2024-01-07 00:04:30 +1030

    test: element delta json serialization

--- src/ProjectPerseusTests/ElementTest.cs
+++ src/ProjectPerseusTests/ElementTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 using ProjectPerseus;
 using ProjectPerseus.models;
 using ProjectPerseus.models.interfaces;
@@ -14,18 +15,8 @@ namespace ProjectPerseusTests
         [TestMethod]
         public void TestFromArdbElementConversion()
         {
-            // Arrange
-            var mockParameters = new List<IArdbParameter>
-            {
-                new MockArdbParameter("1", new MockArdbDefinition("Parameter1"), StorageType.Double, 1.23),
-                new MockArdbParameter("2", new MockArdbDefinition("Parameter2"), StorageType.String, "Test"),
-                new MockArdbParameter("3", null, StorageType.String, "Test"),
-                new MockArdbParameter("4", new MockArdbDefinition("Parameter4"), StorageType.None, null)
-            };
+            var mockParameters = GetMockElement(out var mockElement);
 
-            var mockElement = new MockArdbElement(new MockArdbElementId(1), "UniqueId", "Name", new MockArdbParameterSet(mockParameters));
-
-            // Act
             var element = new Element(mockElement);
 
             // Assert
@@ -36,34 +27,97 @@ namespace ProjectPerseusTests
             for (var i = 0; i < mockParameters.Count; i++)
             {
                 // Assert.AreEqual(mockParameters[i].Guid, element.Parameters[i].Guid, "Parameter guids do not match");
-                Assert.AreEqual(mockParameters[i].Definition?.Name, element.Parameters[i].Name, "Parameter names do not match");
-                
-                Assert.AreEqual(mockParameters[i].Definition?.Name, element.Parameters[i].Name, "Parameter names do not match");
+                Assert.AreEqual(mockParameters[i].Definition?.Name, element.Parameters[i].Name,
+                    "Parameter names do not match");
+
+                Assert.AreEqual(mockParameters[i].Definition?.Name, element.Parameters[i].Name,
+                    "Parameter names do not match");
                 switch (mockParameters[i].StorageType)
                 {
                     case StorageType.Double:
-                        Assert.AreEqual(mockParameters[i].AsDouble(), ((ParameterBase)element.Parameters[i]).Value, "Parameter values do not match");
+                        Assert.AreEqual(mockParameters[i].AsDouble(), ((ParameterBase)element.Parameters[i]).Value,
+                            "Parameter values do not match");
                         break;
                     case StorageType.ElementId:
-                        Assert.AreEqual(mockParameters[i].AsElementId().IntegerValue, ((ParameterBase)element.Parameters[i]).Value, "Parameter values do not match");
+                        Assert.AreEqual(mockParameters[i].AsElementId().IntegerValue,
+                            ((ParameterBase)element.Parameters[i]).Value, "Parameter values do not match");
                         break;
                     case StorageType.Integer:
-                        Assert.AreEqual(mockParameters[i].AsInteger(), ((ParameterBase)element.Parameters[i]).Value, "Parameter values do not match");
+                        Assert.AreEqual(mockParameters[i].AsInteger(), ((ParameterBase)element.Parameters[i]).Value,
+                            "Parameter values do not match");
                         break;
                     case StorageType.String:
-                        Assert.AreEqual(mockParameters[i].AsString(), ((ParameterBase)element.Parameters[i]).Value, "Parameter values do not match");
+                        Assert.AreEqual(mockParameters[i].AsString(), ((ParameterBase)element.Parameters[i]).Value,
+                            "Parameter values do not match");
                         break;
                     case StorageType.None:
-                        Assert.AreEqual(null, ((ParameterBase)element.Parameters[i]).Value, "Parameter values do not match");
+                        Assert.AreEqual(null, ((ParameterBase)element.Parameters[i]).Value,
+                            "Parameter values do not match");
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
                 }
-                Assert.AreEqual(mockParameters[i].StorageType.ToString(), element.Parameters[i].ValueType, "Parameter value types do not match");
+
+                Assert.AreEqual(mockParameters[i].StorageType.ToString(), element.Parameters[i].ValueType,
+                    "Parameter value types do not match");
             }
+        }
+
+        private static List<IArdbParameter> GetMockElement(out MockArdbElement mockElement)
+        {
+            var mockParameters = new List<IArdbParameter>
+            {
+                new MockArdbParameter("1", new MockArdbDefinition("Parameter1"), StorageType.Double, 1.23),
+                new MockArdbParameter("2", new MockArdbDefinition("Parameter2"), StorageType.String, "Test"),
+                new MockArdbParameter("3", null, StorageType.String, "Test"),
+                new MockArdbParameter("4", new MockArdbDefinition("Parameter4"), StorageType.None, null)
+            };
+
+            mockElement = new MockArdbElement(new MockArdbElementId(1), "UniqueId", "Name",
+                new MockArdbParameterSet(mockParameters));
+            return mockParameters;
+        }
+
+
+        [TestMethod]
+        public void TestFromArdbElementToDeltaConversion()
+        {
+            string expectedJson = @"{
+                ""action"": ""Create"",
+                ""element"": {
+                    ""id"": 1,
+                    ""unique_id"": ""UniqueId"",
+                    ""name"": ""Name"",
+                    ""parameters"": [
+                        {
+                            ""name"": ""Parameter1"",
+                            ""value"": 1.23,
+                            ""value_type"": ""Double""
+                        },
+                        {
+                            ""name"": ""Parameter2"",
+                            ""value"": ""Test"",
+                            ""value_type"": ""String""
+                        },
+                        {
+                            ""value"": ""Test"",
+                            ""value_type"": ""String""
+                        },
+                        {
+                            ""name"": ""Parameter4"",
+                            ""value_type"": ""None""
+                        }
+                    ]
+                }
+            }";
+            GetMockElement(out var mockElement);
+            var elementDelta = new ElementDelta(ElementDelta.DeltaAction.Create, mockElement);
+            var actualJson = Utl.SerializeToJson(elementDelta);
             
-            // dump json
-            Utl.JsonDump(element, "ElementTest");
+            var expectedToken = JToken.Parse(expectedJson);
+            var actualToken = JToken.Parse(actualJson);
+
+            Assert.IsTrue(JToken.DeepEquals(expectedToken, actualToken), "JSON does not match");
         }
     }
 }
