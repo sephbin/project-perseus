1022a2b0e80aecfed7ea2cf6c44017acfc189391
Andrew Butler <sephbin@gmail.com>
2025-10-15 13:29:09 +1100

    Update prod.py

--- django_backend/projectperseus/settings/prod.py
+++ django_backend/projectperseus/settings/prod.py
@@ -3,15 +3,3 @@ import os
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sephbin$perseus',
-        'USER': 'sephbin',
-        'PASSWORD': '100%StrongPassword',
-        'HOST': 'sephbin.mysql.pythonanywhere-services.com',
-        'OPTIONS':{
-	        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
-    }
-}
